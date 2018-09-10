8e36307c0cb2797773de7d0caf9f48fa16be3115
Brennan Taylor <brentaylor@expedia.com>
2018-09-10 12:02:08 -0700

    Fix not so simple reporter and expose method to create statsd client from config

--- src/statsd/statsd-utils.ts
+++ src/statsd/statsd-utils.ts
@@ -1,1 +1,16 @@
+import * as StatsdClient from 'statsd-client';
+import { StatsdConfig } from "./statsd-config";
+
 export const sanitizeName = (name: string): string => name.replace(/^\./, '');
+
+const STATSD_HOST_OPTION = 'host';
+const STATSD_PORT_OPTION = 'port';
+const REPORTING_METRIC_PREFIX = 'prefix';
+const STATSD_DEFAULT_HOST = 'localhost';
+const STATSD_DEFAULT_PORT = 8125;
+
+export const defaultStatsd = (config: StatsdConfig): StatsdClient => new StatsdClient({
+    host: config[STATSD_HOST_OPTION] || STATSD_DEFAULT_HOST,
+    port: config[STATSD_PORT_OPTION] || STATSD_DEFAULT_PORT,
+    prefix: config[REPORTING_METRIC_PREFIX] || ''
+  });
